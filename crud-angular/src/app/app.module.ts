e05d693931f64a1970f76655ed25508628ab85aa
Otavio Moreira <otavio_mds@hotmail.com>
2021-09-16 22:39:12 -0300

    Roteamento de cursos + componente de cursos

--- crud-angular/src/app/app.module.ts
+++ crud-angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CoursesModule } from './courses/courses.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    CoursesModule
   ],
   providers: [],
   bootstrap: [AppComponent],
