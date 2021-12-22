cd8f226b05aab9b998127bec7265e2a329a3653f
Otavio Moreira <otavio_mds@hotmail.com>
2021-12-21 22:59:23 -0300

    pipe de categorias

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -9,7 +9,7 @@ import { Course } from '../model/course';
 export class CoursesService {
   courses: Course[] = [{ _id: '1', name: 'Angular', category: 'front-end' }];
 
-  private readonly API = '/assets/acourses.json';
+  private readonly API = '/assets/courses.json';
 
   constructor(private httpClient: HttpClient) {}
 
@@ -17,7 +17,7 @@ export class CoursesService {
     return this.httpClient.get<Course[]>(this.API)
     .pipe(
       take(1), // ou first() finaliza a inscrição após o retorno
-      delay(1000),
+      //delay(1000),
       tap((courses) => console.log(courses))
     );
   }
